9e4207fea5aaa04eee3e8ebbe4ba0fa110265d28
서지훈 <jihunsuh12@gmail.com>
2020-02-05 16:54:57 +0900

    initial setting node.js app

---
+++ README.md
@@ -0,0 +1,4 @@
+# SOBucket-server
+
+# notion document
+https://www.notion.so/2-HaliBoliSky-SO-Bucket-f202ed7fb72745ddb5b44717af06be80
---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+// const FileStore = require("session-file-store")(session);
+
+const morgan = require('morgan');
+
+// const { urls } = require("./models");
+
+const app = express();
+const port = 3001;
+
+app.use(
+  session({
+    secret: '@halibolisky',
+    resave: false,
+    saveUninitialized: true,
+    // ,store: new FileStore()
+  }),
+);
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  }),
+);
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.status(200).send('Success');
+});
+
+app.set('port', port);
+app.listen(app.get('port'), () => {
+  console.log(`app is listening in PORT ${app.get('port')}`);
+});
+
+module.exports = app;
