8156b99357590867e27bd8404f1f4b9ce3f8f5f6
서지훈 <jihunsuh12@gmail.com>
2020-02-06 12:03:07 +0900

    initial setting sequelize connection & add user migration

---
+++ models/user.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    companyid: DataTypes.NUMBER,
+    rank: DataTypes.STRING,
+    completion: DataTypes.STRING
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
