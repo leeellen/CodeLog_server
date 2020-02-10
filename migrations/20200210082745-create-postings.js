43d8947c5f471ee18161b6a3fc74cee54a6b4e77
서지훈 <jihunsuh12@gmail.com>
2020-02-10 19:02:47 +0900

    create service index files

--- migrations/20200210082745-create-postings.js
+++ migrations/20200210082745-create-postings.js
@@ -11,7 +11,6 @@ module.exports = {
       content: {
         type: Sequelize.TEXT,
         allowNull: false,
-        defaultValue: '',
       },
       likes: {
         type: Sequelize.INTEGER,
