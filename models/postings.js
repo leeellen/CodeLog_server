9ae7d319fff5b985faa92df5d43196a3bb936951
서지훈 <jihunsuh12@gmail.com>
2020-02-11 10:19:44 +0900

    change postings and add postings_tag migrations

--- models/postings.js
+++ models/postings.js
@@ -1,11 +1,16 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const postings = sequelize.define('postings', {
-    content: DataTypes.STRING,
-    likes: DataTypes.INTEGER,
-    theme: DataTypes.STRING,
-    userid: DataTypes.INTEGER
-  }, {});
+  const postings = sequelize.define(
+    'postings',
+    {
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      likes: DataTypes.INTEGER,
+      theme: DataTypes.STRING,
+      userid: DataTypes.INTEGER,
+    },
+    {},
+  );
   postings.associate = function(models) {
     // associations can be defined here
   };
