4c0d11ff9474d7b4aba55b8151f4f12aad8152e0
서지훈 <jihunsuh12@gmail.com>
2020-02-14 11:12:42 +0900

    fix error that wrong name (findresult => findResult, .payload)

--- controller/post/board.js
+++ controller/post/board.js
@@ -59,7 +59,7 @@ module.exports = {
       res.status(404).send("i found your postings, but i can't find writer");
       return;
     }
-    const { email, username, rank, completion } = userfindResult;
+    const { email, username, rank, completion } = userfindResult.payload;
     postingInfo['users'] = { email, username, rank, completion };
 
     const tagfindResult = await tags.findNamesByPostId(id);
