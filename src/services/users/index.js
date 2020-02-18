1443f09a646bb7342752d58d018617c684153cb4
서지훈 <jihunsuh12@gmail.com>
2020-02-18 14:12:56 +0900

    test refactoring user

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,153 +1,66 @@
 const { Users } = require('../../models');
+const { handlePromise } = require('../helper');
 
-module.exports = {
-  create: function(email, username, password, companyid, position, completion, website) {
-    console.log(email);
-    return Users.findOrCreate({
-      where: {
-        email,
-      },
-      defaults: {
-        username,
-        password,
-        companyid,
-        position,
-        completion,
-        website,
-      },
-    })
-      .spread((result, created) => {
-        if (!created) {
-          return {
-            success: false,
-            payload: null,
-            message: 'duplicated',
-          };
-        }
-        return {
-          success: true,
-          payload: null,
-          message: 'created',
-        };
-      })
-      .catch((error) => {
-        return {
-          success: false,
-          payload: null,
-          message: error.toString(),
-        };
-      });
-  },
-  find: function(email, password) {
-    return Users.findOne({
-      where: {
-        email,
-      },
-    })
-      .then((result) => {
-        console.log(result.dataValues);
-        if (password !== result.dataValues.password) {
-          return {
-            success: true,
-            payload: null,
-            message: 'wrong password',
-          };
-        }
-        return {
-          success: true,
-          payload: result.dataValues,
-          message: 'exists',
-        };
-      })
-      .catch((error) => {
-        return {
-          success: false,
-          payload: error.toString(),
-          message: 'not exists',
-        };
-      });
-  },
-  findById: function(userid) {
-    return Users.findOne({
-      where: {
-        id: userid,
-      },
-    })
-      .then((result) => {
-        console.log(result.dataValues);
-        return {
-          success: true,
-          payload: result.dataValues,
-          message: 'exists',
-        };
-      })
-      .catch((error) => {
-        return {
-          success: false,
-          payload: error.toString(),
-          message: 'not exists',
-        };
-      });
-  },
-  findByCompany: function(companyid) {
-    return Users.findAll({
-      where: {
-        companyid,
-      },
-    })
-      .then((result) => {
-        return {
-          success: true,
-          payload: result.map((el) => el.dataValues),
-          message: 'exists',
-        };
-      })
-      .catch((error) => {
-        return {
-          success: false,
-          payload: JSON.stringify(error),
-          message: 'not exists',
-        };
-      });
-  },
-  updateByEmail: function(email, username, password, companyid, position, completion, website) {
-    return Users.update(
-      {
-        username,
-        password,
-        companyid,
-        position,
-        completion,
-        website,
-      },
-      {
+export default {
+  create: (email, username, password, companyid, position, completion, website) =>
+    handlePromise(
+      Users.findOrCreate({
         where: {
           email,
         },
-      },
-    )
-      .then((result) => {
-        console.log(result);
-        if (result) {
-          return {
-            success: true,
-            payload: result.dataValues,
-            message: 'updated',
-          };
-        } else {
-          return {
-            success: false,
-            payload: result.dataValues,
-            message: 'not updated',
-          };
-        }
-      })
-      .catch((error) => {
-        return {
-          success: false,
-          payload: null,
-          message: error.toString(),
-        };
-      });
-  },
+        defaults: {
+          email,
+          username,
+          password,
+          companyid,
+          position,
+          completion,
+          website,
+        },
+      }).spread((result, created) => {
+        return created ? 'created' : 'duplicated';
+      }),
+    ),
+  findByEmail: (email) =>
+    handlePromise(
+      Users.findOne({
+        where: {
+          email,
+        },
+      }),
+    ),
+  findByUsername: (username) =>
+    handlePromise(
+      Users.findOne({
+        where: {
+          username,
+        },
+      }),
+    ),
+  findByCompany: (companyid) =>
+    handlePromise(
+      Users.findAll({
+        where: {
+          companyid,
+        },
+      }),
+    ),
+  updateByEmail: (email, username, password, companyid, position, completion, website) =>
+    handlePromise(
+      Users.update(
+        {
+          username,
+          password,
+          companyid,
+          position,
+          completion,
+          website,
+        },
+        {
+          where: {
+            email,
+          },
+        },
+      ),
+    ),
 };
