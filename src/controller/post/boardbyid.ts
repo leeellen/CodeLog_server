fb382eb0a1c05a55cd0e08441dab3a8592728cee
서지훈 <jihunsuh12@gmail.com>
2020-02-22 17:52:26 +0900

    add verify route function

--- src/controller/post/boardbyid.ts
+++ src/controller/post/boardbyid.ts
@@ -3,25 +3,30 @@ const asyncHandler = require('express-async-handler');
 import { Request, Response } from 'express';
 const { postingService, userService } = require('../../services');
 
-import { Result } from '../../interfaces';
+import { Result, PostingRecord } from '../../interfaces';
 
 module.exports = {
   get: asyncHandler(async (req: Request, res: Response) => {
     const id: string = req.params.id;
+    const { token } = req.cookies;
 
     const findresult: Result = await postingService.find(id);
     if (!findresult.success) {
       res.status(404).send("i can't find your postings");
       return;
     }
-    let postingInfo: Object = findresult.payload;
+    let postingInfo: PostingRecord = findresult.payload;
 
-    // const tagfindresult: Result = await tags.findNamesByPostId(id);
-    // if (!tagfindresult.success) {
-    //   res.status(404).send("i found your postings, but i can't find posting tags");
-    //   return;
-    // }
-    // postingInfo['tags'] = tagfindresult.payload;
+    if (token) {
+      const userResult: Result = await userService.findByToken(token);
+      if (!userResult.success) {
+        res.status(403).send('login required');
+        return;
+      }
+      const user_id = userResult.payload.id;
+
+      postingInfo.isAuthor = postingInfo.user_id === user_id;
+    }
     res.status(200).send(postingInfo);
   }),
   delete: asyncHandler(async (req: Request, res: Response) => {
